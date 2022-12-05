ef0356522a2791eec0f215e546a070d4936b265d
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-05 21:33:24 +0100

    Edit message_spec.rb add translate_one_letter

--- ruby/level1/spec/message_spec.rb
+++ ruby/level1/spec/message_spec.rb
@@ -12,9 +12,19 @@ end
 describe "normalize method" do
   let(:blorg_message) {Message.new("BURH LORH GRBL GRBL RGOB", false)}
   let(:french_message) {Message.new("hello", true)}
-  
+
   it "is normalizable" do
     expect(blorg_message.normalize).to eq("burh lorh grbl grbl rgob")
     expect(french_message.normalize).to eq("HELLO")
+  end
+end
+
+describe "translate_one_letter method" do
+  let(:blorg_message) {Message.new("burh", false)}
+  let(:french_message) {Message.new("H", true)}
+
+  it "is translatable" do
+    expect(blorg_message.translate_one_letter("burh")).to eq("H")
+    expect(french_message.translate_one_letter("D")).to eq("olgr")
   end
 end
