dcb8929853417e55596e784f747a722273d78f6c
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-06 00:14:09 +0100

    Edit normalize method to message.rb

--- ruby/level1/message.rb
+++ ruby/level1/message.rb
@@ -39,11 +39,6 @@ class Message
     @is_french = is_french
   end
 
-  # Permet de normaliser l'entrée de la méthode en fonction de la langue.
-  def normalize
-    self.is_french ? self.content.upcase : self.content.downcase
-  end
-
   # Permet d'encoder ou de décoder une lettre
   def translate_one_letter(letter)
     TRANSLATION_TABLE.each do |key, value|
@@ -82,5 +77,12 @@ class Message
       final_message.push(french_word)
     end
     return final_message.join(" ")
+  end
+
+  private
+
+  # Permet de normaliser l'entrée de la méthode en fonction de la langue.
+  def normalize
+    self.is_french ? self.content.to_s.upcase : self.content.to_s.downcase
   end
 end
