4085bb6e11a10fa0af173b63abbcc5b655707b1b
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-05 20:15:06 +0100

    Edit message_spec.rb add normalize method spec

--- ruby/level1/spec/message_spec.rb
+++ ruby/level1/spec/message_spec.rb
@@ -8,3 +8,13 @@ describe "class Message" do
     expect(message.is_french).to eq(true)
   end
 end
+
+describe "normalize method" do
+  let(:blorg_message) {Message.new("BURH LORH GRBL GRBL RGOB", false)}
+  let(:french_message) {Message.new("hello", true)}
+  
+  it "is normalizable" do
+    expect(blorg_message.normalize).to eq("burh lorh grbl grbl rgob")
+    expect(french_message.normalize).to eq("HELLO")
+  end
+end
