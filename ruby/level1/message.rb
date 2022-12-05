11ce6b963521f32442d9f128d4ba3947fe92cfc1
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-05 19:18:39 +0100

    Add class Message

---
+++ ruby/level1/message.rb
@@ -0,0 +1,41 @@
+class Message
+  TRANSLATION_TABLE = {
+    "A"=>"rgbh",
+    "B"=>"brug",
+    "C"=>"bgou",
+    "D"=>"olgr",
+    "E"=>"lorh",
+    "F"=>"gbhu",
+    "G"=>"rlgo",
+    "H"=>"burh",
+    "I"=>"ghob",
+    "J"=>"lrug",
+    "K"=>"rhol",
+    "L"=>"grbl",
+    "M"=>"uhog",
+    "N"=>"glhu",
+    "O"=>"rgob",
+    "P"=>"orgh",
+    "Q"=>"lrob",
+    "R"=>"ogru",
+    "S"=>"uhgb",
+    "T"=>"uhob",
+    "U"=>"hrbo",
+    "V"=>"rgub",
+    "W"=>"ruob",
+    "X"=>"orhu",
+    "Y"=>"ublo",
+    "Z"=>"ohbr",
+    "!"=>"rblg",
+    ","=>"uohr",
+    "."=>"obgl",
+    "?"=>"grlh"
+  }.freeze
+
+  attr_accessor :content, :is_french
+
+  def initialize(content, is_french)
+    @content = content
+    @is_french = is_french
+  end
+end
