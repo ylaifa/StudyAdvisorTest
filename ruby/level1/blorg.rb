55fb55aa934a2fe32c88cb9b0fe0ab6cdfa4b2ed
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-06 00:02:21 +0100

    Add class Blorg and perform method

--- ruby/level1/blorg.rb
+++ ruby/level1/blorg.rb
@@ -1,7 +1,18 @@
-# Ce fichier devra contenir votre système de traduction en langue Blorg.
-# Veuillez vous assurer de tirer le meilleur profit de la programmation object
-# d'assurer une qualite de production
-#
-# Il sera exécuté de la façon suivante:
-# ruby blorg.rb encode '...'
-# ruby blorg.rb decode '...'
+require_relative 'message'
+
+class Blorg
+  def perform
+    if ARGV[0] == "encode"
+      message = Message.new(ARGV[1], true)
+      puts "Voici le message en Blourg :"
+      puts message.encode
+    elsif ARGV[0] == "decode"
+      message = Message.new(ARGV[1], false)
+      puts "Voici le message en Français :"
+      puts message.decode
+    end
+  end
+end
+
+# Permet de lancer le programme.
+Blorg.new.perform
