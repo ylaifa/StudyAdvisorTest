4d8303cdbbabcb634fb72b3b30ed5f5f09aaebb5
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-06 15:16:47 +0100

    Refactor blorg.rb

--- ruby/level1/blorg.rb
+++ ruby/level1/blorg.rb
@@ -4,11 +4,9 @@ class Blorg
   def perform
     if ARGV[0] == "encode"
       message = Message.new(ARGV[1], true)
-      puts "Voici le message en Blourg :"
       puts message.encode
     elsif ARGV[0] == "decode"
       message = Message.new(ARGV[1], false)
-      puts "Voici le message en Français :"
       puts message.decode
     end
   end
