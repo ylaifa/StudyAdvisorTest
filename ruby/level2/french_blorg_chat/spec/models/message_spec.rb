23a15f0a4a0e415d08ea0bf4a2405a15b5ccd761
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-06 23:25:16 +0100

    Spec message model tested

--- ruby/level2/french_blorg_chat/spec/models/message_spec.rb
+++ ruby/level2/french_blorg_chat/spec/models/message_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is creatable" do 
+    message = create(:message)
+    first_message = Message.first
+    expect(first_message.author).to eq(message.author)
+    expect(first_message.content).to eq(message.content)
+    expect(first_message.is_french).to eq(true)
+  end
+
+  it { should validate_presence_of(:author) }
+  it { should validate_presence_of(:content) }
+  it { should validate_presence_of(:is_french) }
 end
