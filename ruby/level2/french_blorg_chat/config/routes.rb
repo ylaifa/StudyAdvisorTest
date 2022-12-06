58f5af71b765f8c6d21ecc7f6f9a57080a9fe14e
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-06 23:55:46 +0100

    Generate messages controller

--- ruby/level2/french_blorg_chat/config/routes.rb
+++ ruby/level2/french_blorg_chat/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+  get 'messages/show'
+  get 'messages/new'
+  get 'messages/create'
+  get 'messages/destoy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
