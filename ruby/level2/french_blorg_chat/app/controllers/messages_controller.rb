4f72802e63848d9fa9ffb37d1c975fd7ba712835
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-07 00:50:03 +0100

    Edit messages controller

--- ruby/level2/french_blorg_chat/app/controllers/messages_controller.rb
+++ ruby/level2/french_blorg_chat/app/controllers/messages_controller.rb
@@ -1,16 +1,35 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all
   end
 
   def show
+    @message = Message.find(params[:id])
   end
 
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.errors.full_messages
+    if @message.save
+      redirect_to root_path
+    else 
+      render 'new'
+    end
   end
 
   def destoy
+    @message = Message.find(params[:id])
+    @message.destoy
+    redirect_to root_path
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:author, :content, :is_french)
   end
 end
