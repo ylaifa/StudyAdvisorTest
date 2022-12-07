a8c50ba465fb675f85ae57489f1a4a26abba9a90
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-07 16:23:53 +0100

    Edit show method to tags controller

--- ruby/level2/french_blorg_chat/app/controllers/tags_controller.rb
+++ ruby/level2/french_blorg_chat/app/controllers/tags_controller.rb
@@ -1,5 +1,7 @@
 class TagsController < ApplicationController
   def show
-    
+    tag_name = params[:name].to_sym
+    tag_id = Tag::TAG_NAMES[tag_name].to_i
+    @tag = Tag.find(tag_id)
   end
 end
