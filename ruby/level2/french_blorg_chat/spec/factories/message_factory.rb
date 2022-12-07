513844c1b69218adc617eb0655769230e8528a12
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-08 00:52:54 +0100

    Edit message factory

--- ruby/level2/french_blorg_chat/spec/factories/message_factory.rb
+++ ruby/level2/french_blorg_chat/spec/factories/message_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :message do
       author { Faker::Movies::BackToTheFuture.character }
-      content { Faker::Movies::BackToTheFuture.quote }
+      content { ["C est un message test", "Il fait beau ici, j espere que chez toi aussi!", "tu rentres quand a la maison, je t ai prepare une tarte a la myrtille"].sample }
       is_french { true }
   end
 end
