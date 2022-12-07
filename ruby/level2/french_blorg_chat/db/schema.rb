5a0487c9f1b120d84a2fa891653e5a44912911d6
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-07 12:34:50 +0100

    Edit and run migration to create tags table

--- ruby/level2/french_blorg_chat/db/schema.rb
+++ ruby/level2/french_blorg_chat/db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_06_212551) do
+ActiveRecord::Schema.define(version: 2022_12_07_111725) do
 
   create_table "messages", force: :cascade do |t|
     t.string "author"
     t.text "content"
     t.boolean "is_french"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
