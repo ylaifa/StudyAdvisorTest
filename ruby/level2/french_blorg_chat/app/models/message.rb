dbcce079be09682eaa4dda2e4c0fb54f73236dc6
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-07 13:25:11 +0100

    Add association Message belong_to tag

--- ruby/level2/french_blorg_chat/app/models/message.rb
+++ ruby/level2/french_blorg_chat/app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
+  belongs_to :tag, optional: true
+
   validates :author, presence: true
   validates :content, presence: true
   validates :is_french, presence: true
