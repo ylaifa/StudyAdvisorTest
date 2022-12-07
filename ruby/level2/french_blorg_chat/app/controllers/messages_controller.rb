f5250759fd4aa85341689e2e59aa27b873aa703a
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-07 22:31:57 +0100

    Edit messages controller

--- ruby/level2/french_blorg_chat/app/controllers/messages_controller.rb
+++ ruby/level2/french_blorg_chat/app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @messages = Message.all.order("created_at DESC")
   end
 
   def show
@@ -12,24 +12,17 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
-    @message.errors.full_messages
+    @message = Message.create(message_params)
     if @message.save
       redirect_to root_path
-    else 
-      render 'new'
+    else
+      render :new
     end
-  end
-
-  def destoy
-    @message = Message.find(params[:id])
-    @message.destoy
-    redirect_to root_path
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:author, :content, :is_french)
+    params.require(:message).permit(:author, :content, :is_french, :tag_id)
   end
 end
