409fded1a501af18fb929d7a787deff0f8dcc538
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-08 00:53:29 +0100

    Spec skip requests test

--- ruby/level2/french_blorg_chat/spec/requests/messages_spec.rb
+++ ruby/level2/french_blorg_chat/spec/requests/messages_spec.rb
@@ -2,35 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "Messages", type: :request do
   describe "GET /index" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/messages/index"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/messages/show"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/messages/new"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /create" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/messages/create"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /destoy" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/messages/destoy"
       expect(response).to have_http_status(:success)
     end
