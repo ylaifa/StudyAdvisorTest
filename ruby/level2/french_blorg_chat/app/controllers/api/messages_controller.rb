fee97c2fd1d9d8f331840701c12db8879cb8bc93
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-08 02:52:54 +0100

    Edit api/messages_controller add index and show

--- ruby/level2/french_blorg_chat/app/controllers/api/messages_controller.rb
+++ ruby/level2/french_blorg_chat/app/controllers/api/messages_controller.rb
@@ -1,3 +1,15 @@
 class Api::MessagesController < ApplicationController
+  def index
+    @messages = Message.all 
+    respond_to do |format|
+      format.json { render json: @messages.to_json, status: :ok }
+    end
+  end
 
+  def show
+    @message = Message.find(params[:id])
+    respond_to do |format|
+      format.json { render json: @message.to_json, status: :ok }
+    end
+  end
 end
