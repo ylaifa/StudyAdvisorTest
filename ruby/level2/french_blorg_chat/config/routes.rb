720410762da1891081b87177649273aa569cd417
Yousri LAÏFA <laifayousri@gmail.com>
2022-12-08 01:24:16 +0100

    Add namespace api to routes.rb

--- ruby/level2/french_blorg_chat/config/routes.rb
+++ ruby/level2/french_blorg_chat/config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :messages, except: [:edit, :update] 
   get 'tags/:name/messages', to: 'tags#show', as: 'tag_name'
 
+  namespace :api do
+    resources :messages, only: [:index, :show] 
+    get 'tags/:name/messages', to: 'tags#show', as: 'tag_name'
+  end
+
   root 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
